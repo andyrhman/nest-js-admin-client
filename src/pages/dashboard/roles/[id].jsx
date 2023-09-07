43b6fea2375ea0a6f272f223cb1de699baf3d508
Andy Rahman <andyrahman989@gmail.com>
2023-09-07 21:42:42 +0800

    adding update roles

--- src/pages/dashboard/roles/[id].jsx
+++ src/pages/dashboard/roles/[id].jsx
@@ -15,8 +15,13 @@ import {
     DialogFooter,
     Input,
     Alert,
-    IconButton,
+    List,
+    ListItem,
+    Checkbox
 } from "@material-tailwind/react";
+
+import { Slide, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 // layout for page
 
@@ -25,16 +30,97 @@ import Sidebar from "@/components/Sidebar/Sidebar.js";
 import HeaderStats from "@/components/Headers/HeaderStats.js";
 import FooterAdmin from "@/components/Footers/FooterAdmin.js";
 import Layout from "@/components/Layout";
-import CreateRole from "./create.roles";
 import { NotificationDeleteDialog } from "@/components/admin/modals/delete.modal";
+import CreateRole from "./create.roles";
 
 export default function EditRoles({ color }) {
-    // Getting the user data
-    const [roles, setRoles] = useState([]);
+    const [name, setName] = useState('');
+    const [permissions, setPermissions] = useState([]);
+    const [selected, setSelected] = useState([]);
 
     const [error, setError] = useState('');
 
+    const [roles, setRoles] = useState([])
+
     const router = useRouter();
+    const { id } = router.query;
+    useEffect(() => {
+        if (id) {
+            (
+                async () => {
+                    try {
+                        const response = await http.get('/permissions');
+                        setPermissions(response.data);
+
+                        const {data} = await http.get(`/roles/${id}`);
+                        
+                        setName(data.name);
+                        setSelected(data.permissions.map((permission) => permission.id));
+                    } catch (error) {
+                        if (error.response && error.response.status === 401) {
+                            setError('An error occurred');
+                            router.push('/login');
+                        }
+
+                        if (error.response && error.response.status === 403) {
+                            setError('An error occurred');
+                            router.push('/login');
+                        }
+                    }
+
+                }
+            )()
+        }
+
+    }, [id]);
+
+    const check = (id) => {
+        if (selected.some(s => s === id)) {
+            setSelected(selected.filter(s => s !== id));
+            return;
+        }
+        setSelected([...selected, id]);
+    }
+
+    const submit = async (e) => {
+        e.preventDefault();
+        setError('');
+
+        try {
+            const { data } = await http.put(`/roles/${id}`, {
+                name,
+                permissions: selected
+            });
+            if (data) {
+                // Show a success message using React Toastify
+                toast.success('Role Updated Successfully!.', {
+                    position: "top-center",
+                    autoClose: 2000,
+                    hideProgressBar: true,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "colored",
+                    transition: Slide
+                });
+                setTimeout(() => {
+                    router.push('/dashboard/roles');
+                }, 2000);
+                setOpen(!open);
+            } else {
+                // Sign-in failed, display an error message
+                setError('An error occurred during sign-in');
+            }
+        } catch (error) {
+            console.error(error.response);
+            if (error.response && error.response.data && error.response.data.message) {
+                const errorMessage = error.response.data.message;
+                setError(errorMessage);
+            }
+        }
+
+    }
 
     useEffect(() => {
 
@@ -89,7 +175,8 @@ export default function EditRoles({ color }) {
         handleOpenDialog();
     };
 
-    // Create Modal
+
+    // * Create Modal
     const [isModalOpenCreate, setIsModalOpenCreate] = useState(false);
 
     const openModalCreate = () => {
@@ -100,53 +187,13 @@ export default function EditRoles({ color }) {
         setIsModalOpenCreate(false);
     };
 
-    // Edit Modal
+    // * Edit Modal
     const [open, setOpen] = useState(true);
 
     const handleOpen = () => {
         router.push('/dashboard/roles');
         setOpen(!open);
     };
-
-    const submit = async (e) => {
-        e.preventDefault();
-        setError('');
-
-        try {
-            const { data } = await http.post('/roles', {
-                name,
-                permissions: selected
-            });
-            if (data) {
-                // Show a success message using React Toastify
-                toast.success('Role Created Successfully!.', {
-                    position: "top-center",
-                    autoClose: 2000,
-                    hideProgressBar: true,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "colored",
-                    transition: Slide
-                });
-                setTimeout(() => {
-                    window.location.reload();
-                }, 2000);
-                onClose();
-            } else {
-                // Sign-in failed, display an error message
-                setError('An error occurred during sign-in');
-            }
-        } catch (error) {
-            console.error(error.response);
-            if (error.response && error.response.data && error.response.data.message) {
-                const errorMessage = error.response.data.message;
-                setError(errorMessage);
-            }
-        }
-
-    }
 
     return (
         <Layout>
@@ -227,55 +274,6 @@ export default function EditRoles({ color }) {
                                         ))}
 
                                     </table>
-                                    <Dialog open={open} handler={handleOpen}>
-                                        <div className="flex items-center justify-between">
-                                            <DialogHeader>Create New User</DialogHeader>
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                viewBox="0 0 24 24"
-                                                fill="currentColor"
-                                                className="mr-3 h-5 w-5"
-                                                onClick={handleOpen} // Change this line
-                                            >
-                                                <path
-                                                    fillRule="evenodd"
-                                                    d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z"
-                                                    clipRule="evenodd"
-                                                />
-                                            </svg>
-                                        </div>
-                                        <form onSubmit={submit}>
-                                            <DialogBody divider>
-                                                <div className="grid gap-6">
-                                                    {error && (
-                                                        <Alert color="red" className="mt-2 text-center font-normal">
-                                                            {error}
-                                                        </Alert>
-                                                    )}
-
-                                                    <div className="mb-5">
-                                                        <Input
-                                                            label="Name"
-                                                            required
-                                                            onChange={(e) => setName(e.target.value)}
-
-                                                        />
-                                                    </div>
-
-
-
-                                                </div>
-                                            </DialogBody>
-                                            <DialogFooter className="space-x-2">
-                                                <Button variant="outlined" color="red" onClick={handleOpen}>
-                                                    close
-                                                </Button>
-                                                <Button variant="gradient" color="blue" type='submit'>
-                                                    Submit
-                                                </Button>
-                                            </DialogFooter>
-                                        </form>
-                                    </Dialog>
                                     <CreateRole isOpen={isModalOpenCreate} onClose={closeModalCreate} />
                                     <NotificationDeleteDialog open={openDialog} handleOpenDelete={handleOpenDialog} handleConfirmDelete={handleConfirmDelete} />
                                 </div>
@@ -284,6 +282,80 @@ export default function EditRoles({ color }) {
                     </div>
                 </div>
             </div>
+            <Dialog open={open} handler={handleOpen}>
+                <div className="flex items-center justify-between">
+                    <DialogHeader>Edit Role</DialogHeader>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        className="mr-3 h-5 w-5"
+                        onClick={handleOpen}
+                    >
+                        <path
+                            fillRule="evenodd"
+                            d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z"
+                            clipRule="evenodd"
+                        />
+                    </svg>
+                </div>
+                <form onSubmit={submit}>
+                    <DialogBody divider>
+                        <div className="grid gap-6">
+                            {error && (
+                                <Alert color="red" className="mt-2 text-center font-normal">
+                                    {error}
+                                </Alert>
+                            )}
+
+                            <div className="mb-5">
+                                <Input
+                                    label="Name"
+                                    required
+                                    defaultValue={name}
+                                    onChange={(e) => setName(e.target.value)}
+
+                                />
+                            </div>
+
+                            <div className="mb-5">
+                                <Typography color="blue-gray" className="font-medium">
+                                    Permissions
+                                </Typography>
+                                <List className="grid grid-cols-3 gap-3">
+                                    {permissions.map((permission) => (
+                                        <ListItem key={permission.id}>
+                                            <Checkbox
+                                                color="blue"
+                                                ripple={false}
+                                                className="hover:before:opacity-0"
+                                                containerProps={{
+                                                    className: "p-2",
+                                                }}
+                                                label={permission.name}
+                                                value={permission.id}
+                                                checked={selected.some(s => s === permission.id)}
+                                                onChange={() => check(permission.id)}
+                                            />
+                                        </ListItem>
+                                    ))}
+                                </List>
+
+
+                            </div>
+
+                        </div>
+                    </DialogBody>
+                    <DialogFooter className="space-x-2">
+                        <Button variant="outlined" color="red" onClick={handleOpen}>
+                            Close
+                        </Button>
+                        <Button variant="gradient" color="blue" type='submit'>
+                            Submit
+                        </Button>
+                    </DialogFooter>
+                </form>
+            </Dialog>
             <FooterAdmin />
         </Layout>
     );
